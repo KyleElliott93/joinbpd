adef0a1381f028bf067db0f38fa8cf57416184db
Kyle <kyleelliott10893@gmail.com>
2026-01-27 18:29:34 -0600

    Initial project setup.

---
+++ src/pages/Apply.jsx
@@ -0,0 +1,32 @@
+function Apply() {
+    return (
+        <div style={{
+            padding: '4rem 2rem',
+            minHeight: '80vh'
+        }}>
+            <div style={{
+                maxWidth: '1200px',
+                margin: '0 auto'
+            }}>
+                <h1 style={{
+                    fontSize: '3rem',
+                    fontWeight: 'bold',
+                    color: '#1e3a8a',
+                    marginBottom: '2rem'
+                }}>
+                    Apply Now
+                </h1>
+                <p style={{
+                    fontSize: '1.25rem',
+                    color: '#6b7280',
+                    marginBottom: '2rem'
+                }}>
+                    Start your journey with Buda Police Department.
+                </p>
+                {/* Add application form/link here later */}
+            </div>
+        </div>
+    )
+}
+
+export default Apply
